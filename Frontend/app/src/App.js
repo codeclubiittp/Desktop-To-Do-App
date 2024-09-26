9d238bb84899868e8ce500854886b7ca109646f7
i_apex <jaim7viramgama@gmail.com>
2024-09-26 21:40:15 +0530

    sidebar added

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from 'react';
+import Sidebar from './components/sidebar';
 
 function App() {
-    const [message, setMessage] = useState('');
+    // const [message, setMessage] = useState('');
 
-    useEffect(() => {
-        window.api.receive('response-channel', (message) => {
-            setMessage(message);
-        });
-    }, []);
+    // useEffect(() => {
+    //     window.api.receive('response-channel', (message) => {
+    //         setMessage(message);
+    //     });
+    // }, []);
 
-    const sendMessage = () => {
-        window.api.send('message-channel', 'Hello from React!');
-    };
+    // const sendMessage = () => {
+    //     window.api.send('message-channel', 'Hello from React!');
+    // };
 
     return (
         <div>
-            <button onClick={sendMessage}>Send Message</button>
-            <p>Message from Electron: {message}</p>
+            {/* <button onClick={sendMessage}>Send Message</button>
+            <p>Message from Electron: {message}</p> */}
+
+            <Sidebar />
         </div>
     );
 }
